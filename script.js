6c48b601745abfd27f0cdfdf364025c0b1e8d33e
Steven <s.emmelkamp@gmail.com>
2023-04-30 21:51:30 +0200

    Add Quotes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('it just works')
+
+const arrayFirstPart = ["The sky is not blue, ", "Pizza is not a food, ", "Laughter is not the best medicine, ", "The Earth is not round, ", "Water is not wet, "];
+const arraySecondPart = ["it's just a figment of our imagination. ", "it's a state of mind. ", "it's just a placebo effect. ", "it's actually a triangle. ", "it's just an illusion created by our senses. "];
+const arrayThirdPart = ["Therefore, techno music is a way to release pent-up energy and feel alive.", "Therefore, techno music is not just about the beat, it's about the way it makes you feel.", "Therefore, techno music is a journey of self-discovery and exploration.", "Therefore, techno music is a sonic landscape that allows you to escape from reality.", "Therefore, techno music is a powerful force that brings people together and creates a sense of community."];
